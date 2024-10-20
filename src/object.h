8ba423b6a8517dee4915be7ebc27747c0116fe5b
unknown <mark123ymm@gmail.com>
2024-10-20 18:14:28 -0400

    generic object header, math header

---
+++ src/object.h
@@ -0,0 +1,22 @@
+#ifndef _OBJECT_
+#define _OBJECT_
+#include "raylib.h"
+#include "raymath.h"
+#include "plane.h"
+#include "logger.h"
+#include <iostream>
+#include <vector>
+
+class Object {
+    std::vector<Vector3> state;
+    std::vector<Vector3> state0; // initial states
+    
+    std::vector<Plane> &colliders;
+    FileLogger &logger;
+public:
+    Object(std::vector<Vector3> &state0);
+    virtual std::vector<Vector3> F(std::vector<Vector3> &S, float timestep, float time);
+    virtual std::string stringify() = 0;
+};
+
+#endif
